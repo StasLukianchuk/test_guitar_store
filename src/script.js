a16744a04918cd5776e7138e73f0334c70d32757
Stas Lukianchuk <stas.lukianchuk@icloud.com>
2023-09-25 18:54:38 +0300

    burger menu and adaptive header

--- src/script.js
+++ src/script.js
@@ -1,3 +1,11 @@
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
 document.addEventListener("DOMContentLoaded", function() {
   let orderForm = document.getElementById("orderForm");
   let nameInput = document.getElementById("name");
